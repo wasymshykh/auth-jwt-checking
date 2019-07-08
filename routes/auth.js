e01af29402e24c0c76ffd35d6dbbeea3f208781c
Wasym Shykh <wasymshykh@gmail.com>
2019-07-08 22:18:38 +0500

    Register Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (request, response) => {
+    // Data Validation
+    const { error } = registerValidation(request.body);
+    if (error) {
+        return response.status(400).send(error.details[0].message);
+    }
+
+    // User existance check
+    const emailExist = await User.findOne({ email: request.body.email });
+
+    if (emailExist) {
+        return response.status(400).send("Email already exists");
+    }
+
+    // Create User
     const user = new User({
         name: request.body.name,
         email: request.body.email,
